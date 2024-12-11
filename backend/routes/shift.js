75f7307819ecc59acc0c4630beffa8cd92ce3989
Wai Phyo Paing <waiphyopaing@wais-air.lan>
2024-12-10 22:38:30 -0800

    Day Two: Database integration with PostgreSQL and MongoDB

--- backend/routes/shift.js
+++ backend/routes/shift.js
@@ -1,8 +1,25 @@
 import express from "express";
+import { pool, TestModel } from "../server.js";
 const router = express.Router();
 
-router.get('/shifts', (req, res) => {
-    res.json({message: 'List of shifts will go here.'});
+router.get('/shifts', async (req, res) => {
+    // res.json({message: 'List of shifts will go here.'});
+    console.log('this is shifts route');
+    try{
+        const result = await pool.query('SELECT * FROM shifts');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
+
+router.post('/messages', async (req, res) => {
+    try{
+        const message = await TestModel.create(req.body);
+        res.json(message);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,63 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import pkg from "pg";
+const { Pool } = pkg;
+
 // Routes
 import shiftRouters from "./routes/shift.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Postgres
+export const pool = new Pool({
+    user: process.env.POSTGRES_DB,
+    host: 'postgres',
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+});
+
+// Mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected.'))
+    .catch(err => console.error('Error connecting to MongoDBL:', err));
+
+pool.query('SELECT NOW()', (err, res) => {
+    if(err) {
+        console.error('Error connecting to PostgresSQL:', err);
+    }else {
+        console.log('PostgreSQL connected:', res.rows);
+    }
+});
+
+// Mongodb test
+const TestSchema = new mongoose.Schema({
+    name: String,
+    createdAt: { type: Date, default: Date.now },
+});
+
+export const TestModel = mongoose.model('Test', TestSchema);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Backend is running.');
+});
+
+app.get('/test', (req, res) => {
+    // Postgres test
+    pool.query('SELECT NOW()', (err, res) => {
+        console.log(res.rows);
+    });
+
+    TestModel.create({ name: 'Test Data' }).then(doc => console.log(doc));
+
+    console.log('test');
+    res.send('Test route is running.');
 });
 
 app.use('/api', shiftRouters);
